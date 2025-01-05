691e804b964ff85ed2ffbee4902894335d222a9e
Rodolfo Oliveira <orodolfoso.dev@gmail.com>
2025-01-05 09:41:46 -0300

    Slide finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,22 @@ function menuShow() {
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 }
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => { 
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
